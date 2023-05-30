c4185e9e6dbbd29dbb9379108e23125ea6bfaac9
iakev <kirimikmwongera@gmail.com>
2023-05-30 12:46:33 -0700

    Added a template class for the authentication system

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+"""
+Module that creates a class to manage
+API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Class template for the authentication
+    system
+    """
+    def require_auth(self, path: str, excluded_path: List[str]) -> bool:
+        """
+        Returns False according to path and excluded_path
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns None or str accrding to request
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns None or User according to request
+        """
+        return None
