5ecbb6f6749fce09740cac76a7e6ccfd7a3b5a2e
iakev <kirimikmwongera@gmail.com>
2023-05-31 07:43:52 -0700

    Modfied for type annotation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,10 +5,25 @@ inheriting from Auth and implementing
 BasicAuth for REST API
 """
 from api.v1.auth.auth import Auth
+from typing import Optional
 
 
 class BasicAuth(Auth):
     """
     Contains BasicAuth implementation
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> Optional[str]:
+        """
+        Returns the Base64 part of authorization header
+        for Basic authentication
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        auth_list = authorization_header.split(" ")
+        if auth_list[0] != 'Basic':
+            return None
+        else:
+            return auth_list[1]
