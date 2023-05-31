e05e4026fe448b90f37a890a120ca3be43472ee2
iakev <kirimikmwongera@gmail.com>
2023-05-31 07:15:16 -0700

    Modified to make the BasicAuth auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,12 +12,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth = os.getenv('AUTH_TYPE')
-if auth == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-if auth and auth != 'basic_auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+if auth:
+    if auth == 'basic_auth':
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    elif auth != 'basic_auth':
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 
 @app.errorhandler(404)
