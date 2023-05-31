c5dbe76ec6e75c3c70d8cc7c193b5c13d29ad381
iakev <kirimikmwongera@gmail.com>
2023-05-31 06:54:52 -0700

    Added a class that implements BasicAuth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module containing class BasicAuth
+inheriting from Auth and implementing
+BasicAuth for REST API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Contains BasicAuth implementation
+    """
+    pass
