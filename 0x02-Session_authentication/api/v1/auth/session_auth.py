b11ce6a27fd998dce4f2bad60b7fddd01b8e56bc
iakev <kirimikmwongera@gmail.com>
2023-06-01 05:57:29 -0700

    Modified to create an in memory session ID string, to be able to retrieve a User id based on a Session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,8 @@ inheriting from Auth and implementing
 Session Authentication
 """
 from api.v1.auth.auth import Auth
+from typing import Optional
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -12,4 +14,13 @@ class SessionAuth(Auth):
     Contains Session Authentication
     implementation
     """
-    pass
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> Optional[str]:
+        """
+        Creates a Session ID for a user_id
+        """
+        if not user_id or type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
