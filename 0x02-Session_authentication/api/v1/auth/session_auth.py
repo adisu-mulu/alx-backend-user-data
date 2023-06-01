ebe3766f2fb56e247f09540f16a40b4e172b2582
iakev <kirimikmwongera@gmail.com>
2023-06-01 05:39:06 -0700

    Setting up Session authentication mechanism

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module containing class SeesionAuth
+inheriting from Auth and implementing
+Session Authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Contains Session Authentication
+    implementation
+    """
+    pass
