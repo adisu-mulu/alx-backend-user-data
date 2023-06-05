264e71ee7fa9d565e538b78ffcea4caa1d8aaf4f
iakev <kirimikmwongera@gmail.com>
2023-06-05 05:26:16 -0700

    Added method to validate user login via email and password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,3 +43,17 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Locates user by email, if existing checks password
+        if password is valid returns True otherwise False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode(), user.hashed_password):
+                return True
+            else:
+                return False
+        except (InvalidRequestError, NoResultFound):
+            return False
